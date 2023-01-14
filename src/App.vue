3749067f2ace5aa300a6ae8aaafe6fc937ed3302
Axtazer <jeanpierre.castaldo@gmail.com>
2023-01-14 23:08:02 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -53,7 +53,7 @@ export default {
   <ul>
     <li v-for="todo in todos" :key="todo.id">
       {{ todo.text }}
-      <button @click="removeTodo(todo)"><span class="material-symbols-outlined"></span></button>
+      <button @click="removeTodo(todo)"><span class="material-symbols-outlined">CLOSE</span></button>
     </li>
   </ul>
 </template>
